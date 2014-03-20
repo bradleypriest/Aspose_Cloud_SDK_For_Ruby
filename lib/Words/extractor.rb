cb62a6f0f0a3f08dd095722ee49052b823178119
Assad Mahmood Qazi <assadvirgo@gmail.com>
2014-03-20 15:50:17 +0500

    Code cleanup and enhancements of Words Extractor

--- lib/Words/extractor.rb
+++ lib/Words/extractor.rb
@@ -1,68 +1,41 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 module Aspose
   module Cloud
     module Words
       class Extractor
-        def initialize filename
+        def initialize(filename)
           @filename = filename
-          raise 'Base file not specified.' if @filename.empty?
+          raise 'filename not specified.' if filename.empty?
+          @base_uri = "#{Aspose::Cloud::Common::Product.product_uri}/words/#{@filename}"
         end
 
 =begin
    Gets Text items list from document
 =end
-
-        def get_text
-
-
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/words/' + @filename + '/textItems'
-            signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-
-            response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-
-            stream_hash = JSON.parse(response_stream)
-            stream_hash['Code'] == 200 ? stream_hash['TextItems']['List'] : false
-
-
-
+        def get_text(folder_name = '', storage_type = 'Aspose', storage_name = '')
+          str_uri = "#{@base_uri}/textItems"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          JSON.parse(RestClient.get(signed_str_uri, {:accept=>'application/json'}))['TextItems']['List']
         end
 
 =begin
    Get the OLE drawing object from document
    @param number index
-   @param string ole_format
+   @param string output_format
 =end
 
-        def get_ole_data index, ole_format
+        def get_ole_data(ole_index, ole_format, folder_name = '', storage_type = 'Aspose', storage_name = '')
+          raise 'ole_index not specified.' if ole_index.nil?
+          raise 'ole_format not specified.' if ole_format.empty?
 
+          str_uri = "#{@base_uri}/drawingObjects/#{ole_index}/oleData"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          response_stream = RestClient.get(signed_str_uri, {:accept=>'application/json'})
 
-
-            if index == ''
-              raise 'Index not specified.'
-            end
-
-            if ole_format == ''
-              raise 'OLE Format not specified.'
-            end
-
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/words/' + @filename + '/drawingObjects/' + index.to_s + '/oleData'
-            signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-            response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-
-            valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
-
-            if valid_output == ''
-
-              output_path = Aspose::Cloud::Common::AsposeApp.output_location + Aspose::Cloud::Common::Utils.get_filename(@filename) + '_' + index.to_s + '.' + ole_format
-              Aspose::Cloud::Common::Utils.save_file(response_stream, output_path)
-              return output_path
-            else
-              return valid_output
-            end
-
-
-
+          valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
+          output_path = "#{Aspose::Cloud::Common::AsposeApp.output_location}#{Aspose::Cloud::Common::Utils.get_filename(@filename)}_#{ole_index}.#{ole_format}"
+          valid_output.empty? ? Aspose::Cloud::Common::Utils.save_file(response_stream,output_path) : valid_output
         end
 
 =begin
@@ -70,37 +43,18 @@ module Aspose
    @param number index
    @param string render_format
 =end
+        def get_image_data(image_index, image_format, folder_name = '', storage_type = 'Aspose', storage_name = '')
+          raise 'image_index not specified.' if image_index.nil?
+          raise 'image_format not specified.' if image_format.empty?
 
-        def get_image_data index, render_format
+          str_uri = "#{@base_uri}/drawingObjects/#{image_index}/imagedata"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          response_stream = RestClient.get(signed_str_uri, {:accept=>'application/json'})
 
-
-
-            if index == ''
-              raise 'Index not specified.'
-            end
-
-            if render_format == ''
-              raise 'Render Format not specified.'
-            end
-
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/words/' + @filename + '/drawingObjects/' + index.to_s + '/imagedata'
-            signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-
-            response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-
-            valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
-
-            if valid_output == ''
-
-              output_path = Aspose::Cloud::Common::AsposeApp.output_location + Aspose::Cloud::Common::Utils.get_filename(@filename) + '_' + index.to_s + '.' + render_format
-              Aspose::Cloud::Common::Utils.save_file(response_stream, output_path)
-              return output_path
-            else
-              return valid_output
-            end
-
-
-
+          valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
+          output_path = "#{Aspose::Cloud::Common::AsposeApp.output_location}#{Aspose::Cloud::Common::Utils.get_filename(@filename)}_#{image_index}.#{image_format}"
+          valid_output.empty? ? Aspose::Cloud::Common::Utils.save_file(response_stream,output_path) : valid_output
         end
 
 =begin
@@ -108,56 +62,28 @@ module Aspose
    @param number index
    @param string render_format
 =end
+        def convert_drawing_object(object_index, render_format, folder_name = '', storage_type = 'Aspose', storage_name = '')
+          raise 'object_index not specified.' if object_index.nil?
+          raise 'render_format not specified.' if render_format.empty?
 
-        def convert_drawing_object index, render_format
+          str_uri = "#{@base_uri}/drawingObjects/#{object_index}?format=#{render_format}"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          response_stream = RestClient.get(signed_str_uri, {:accept=>'application/json'})
 
-
-
-            if index == ''
-              raise 'Index not specified.'
-            end
-
-            if render_format == ''
-              raise 'Render Format not specified.'
-            end
-
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/words/' + @filename + '/drawingObjects/' + index.to_s + '?format=' + render_format
-            signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-
-            response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-
-            valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
-
-            if valid_output == ''
-
-              output_path = Aspose::Cloud::Common::AsposeApp.output_location + Aspose::Cloud::Common::Utils.get_filename(@filename) + '_' + index.to_s + '.' + render_format
-              Aspose::Cloud::Common::Utils.save_file(response_stream, output_path)
-              return output_path
-            else
-              return valid_output
-            end
-
-
-
+          valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
+          output_path = "#{Aspose::Cloud::Common::AsposeApp.output_location}#{Aspose::Cloud::Common::Utils.get_filename(@filename)}_#{object_index}.#{render_format}"
+          valid_output.empty? ? Aspose::Cloud::Common::Utils.save_file(response_stream,output_path) : valid_output
         end
 
 =begin
    Get the List of drawing object from document	
 =end
-
-        def get_drawing_object_list
-
-
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/words/' + @filename + '/drawingObjects'
-            signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-
-            response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-
-            stream_hash = JSON.parse(response_stream)
-            stream_hash['Code'] == 200 ? stream_hash['DrawingObjects']['List'] : false
-
-
-
+        def get_drawing_object_list(folder_name = '', storage_type = 'Aspose', storage_name = '')
+          str_uri = "#{@base_uri}/drawingObjects"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          JSON.parse(RestClient.get(signed_str_uri, {:accept=>'application/json'}))['DrawingObjects']['List']
         end
 
 =begin
@@ -166,94 +92,45 @@ module Aspose
    @param string output_path
 =end
 
-        def get_drawing_object object_uri, output_path
+        def get_drawing_object(object_uri, output_path, folder_name = '', storage_type = 'Aspose', storage_name = '')
+          raise 'object_uri not specified.' if object_uri.empty?
+          raise 'output_path not specified.' if output_path.empty?
 
+          object_index = object_uri[-1]
+          str_uri =  "#{Aspose::Cloud::Common::Product.product_uri}/words/#{object_uri}"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          object_info = JSON.parse(RestClient.get(signed_str_uri, {:accept=>'application/json'}))
 
+          str_uri = "#{Aspose::Cloud::Common::Product.product_uri}/words/#{object_uri}?format=jpeg"
+          output_path = "#{output_path}/DrawingObject_#{object_index}.jpeg"
 
-            if object_uri == ''
-              raise 'Object URI not specified.'
-            end
+          str_uri = "#{Aspose::Cloud::Common::Product.product_uri}/words/#{object_uri}/imageData" unless object_info['DrawingObject']['ImageDataLink'].nil?
+          output_path = "#{output_path}/DrawingObject_#{object_index}.jpeg" unless object_info['DrawingObject']['ImageDataLink'].nil?
 
-            if output_path == ''
-              raise 'Output path not specified.'
-            end
+          str_uri = "#{Aspose::Cloud::Common::Product.product_uri}/words/#{object_uri}/oleData" unless object_info['DrawingObject']['OLEDataLink'].nil?
+          output_path = "#{output_path}/DrawingObject_#{object_index}.xlsx" unless object_info['DrawingObject']['OLEDataLink'].nil?
 
-            url_arr = object_uri.split('/')
-            object_index = url_arr.at(-1)
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          response_stream = RestClient.get(signed_str_uri, {:accept=>'application/json'})
 
-            str_uri = object_uri
-            signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-
-            response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-
-            stream_hash = JSON.parse(response_stream)
-
-            if stream_hash['Code'] == 200
-
-              if stream_hash['DrawingObject']['ImageDataLink'] != ''
-                str_uri = str_uri + '/imageData'
-                output_path = output_path + '\\DrawingObject_' + object_index.to_s + '.jpeg'
-              elsif stream_hash['DrawingObject']['OLEDataLink'] != ''
-                str_uri = str_uri + '/oleData'
-                output_path = output_path + '\\DrawingObject_' + object_index.to_s + '.xlsx'
-              else
-                str_uri = str_uri + '?format=jpeg'
-                output_path = output_path + '\\DrawingObject_' + object_index.to_s + '.jpeg'
-              end
-
-              signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-              response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-              valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
-
-              if valid_output == ''
-
-                # output_path = Aspose::Cloud::Common::AsposeApp.output_location + Aspose::Cloud::Common::Utils.get_filename(@filename) + '_' + index.to_s + '.' + render_format
-                Aspose::Cloud::Common::Utils.save_file(response_stream, output_path)
-                return output_path
-              else
-                return valid_output
-              end
-
-            else
-              return false
-            end
-
-
-
+          valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
+          valid_output.empty? ? Aspose::Cloud::Common::Utils.save_file(response_stream,output_path) : valid_output
         end
 
 =begin
    Get the List of drawing object from document
    @param string output_path
 =end
+        def get_drawing_objects(output_path, folder_name = '', storage_type = 'Aspose', storage_name = '')
+          raise 'output_path not specified.' if output_path.empty?
 
-        def get_drawing_objects output_path
-
-
-
-            if output_path == ''
-              raise 'Output path not specified.'
-            end
-
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/words/' + @filename + '/drawingObjects'
-            signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-
-            response_stream = RestClient.get(signed_str_uri, {:accept => 'application/json'})
-
-            stream_hash = JSON.parse(response_stream)
-
-            return false unless stream_hash['Code'] == 200
-
-            stream_hash['DrawingObjects']['List'].each do |object|
-              self.get_drawing_object(object['link']['Href'], output_path)
-            end
-
-
-
+          get_drawing_object_list(folder_name,storage_type,storage_name).each{ |obj|
+            self.get_drawing_object(obj['link']['Href'],output_path)
+          }
         end
-
       end
     end
-
   end
 end
