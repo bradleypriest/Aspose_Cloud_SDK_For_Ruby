c7fbd72d6f71f91ce666a1e4287ed5e0e6e8e42e
Assad Mahmood Qazi <assadvirgo@gmail.com>
2014-03-21 09:38:41 +0500

    Code cleanup and enhancements of Cell ChartEditor

--- lib/Cells/chart_editor.rb
+++ lib/Cells/chart_editor.rb
@@ -1,135 +1,74 @@
 module Aspose
   module Cloud
     module Cells
-      # This class contains features to work with charts
       class ChartEditor
-        def initialize filename,worksheet_name
+        def initialize(filename)
           @filename = filename
-          @worksheet_name = worksheet_name
+          raise 'filename not specified.' if filename.empty?
+          @base_uri = "#{Aspose::Cloud::Common::Product.product_uri}/cells/#{@filename}"
         end
-    
-        # Adds a new chart 
-        # chartType
-        # upperLeftRow
-        # upperLeftColumn
-        # lowerRightRow
-        # lowerRightColumn
-    
-        def add_chart chart_type, upper_left_row, upper_left_column, lower_right_row, lower_right_column
 
-            if @filename==''
-              raise 'Base file not specified'
-            end
-            if @worksheet_name==''
-              raise 'Worksheet is not specified'
-            end
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/cells/' + @filename + '/worksheets/' + @worksheet_name + '/charts?chartType=' +
-              chart_type.to_s + '&upperLeftRow=' + upper_left_row.to_s + '&upperLeftColumn=' +
-              upper_left_column.to_s + '&lowerRightRow=' + lower_right_row.to_s + 
-              '&lowerRightColumn=' + lower_right_column.to_s
-            signed_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-          
-            response = RestClient.put signed_uri,'', {:accept => 'application/json'}
-            v_output = Aspose::Cloud::Common::Utils.validate_output(response)
-            if v_output==nil || v_output==''
-              folder = Aspose::Cloud::AsposeStorage::Folder.new
-              outputstream = folder.get_file(@filename)              
-              outputpath = Aspose::Cloud::Common::AsposeApp.output_location + @filename
-              Aspose::Cloud::Common::Utils.save_file(outputstream, outputpath)
-              return 'Chart added'
-            else
-              return v_output
-            end
+        def add_chart(worksheet_name, chart_type, upper_left_row, upper_left_column, lower_right_row, lower_right_column, folder_name='', storage_type = 'Aspose', storage_name = '')
+          raise 'worksheet_name not specified.' if worksheet_name.empty?
+          raise 'chart_type not specified.' if chart_type.empty?
+          raise 'upper_left_row not specified.' if upper_left_row.nil?
+          raise 'upper_left_column not specified.' if upper_left_column.nil?
+          raise 'lower_right_row not specified.' if lower_right_row.nil?
+          raise 'lower_right_column not specified.' if lower_right_column.nil?
 
+          str_uri = "#{@base_uri}/worksheets/#{worksheet_name}/charts"
+          qry = { :chartType => chart_type, :upperLeftRow => upper_left_row, :upperLeftColumn => upper_left_column,
+                  :lowerRightRow => lower_right_row, :lowerRightColumn => lower_right_column }
+          str_uri = Aspose::Cloud::Common::Utils.build_uri(str_uri,qry)
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          response_stream = RestClient.put(signed_str_uri,'',{:accept=>'application/json'})
+          valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
+          valid_output.empty? ? Aspose::Cloud::Common::Utils.download_file(@filename,@filename,folder_name,storage_name,storage_type) : valid_output
         end
-    
-        # Deletes a chart 
-        # chart_index
-    
-        def delete_chart chart_index
 
-            if @filename==''
-              raise 'Base file not specified'
-            end
-            if @worksheet_name==''
-              raise 'Worksheet is not specified'
-            end
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/cells/' + @filename + '/worksheets/' + @worksheet_name + '/charts/' + chart_index.to_s
-            signed_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
-          
-            response = RestClient.delete signed_uri, {:accept => 'application/json'}
-            v_output = Aspose::Cloud::Common::Utils.validate_output(response)
-            if v_output==nil || v_output==''
-              folder = Aspose::Cloud::AsposeStorage::Folder.new
-              outputstream = folder.get_file(@filename)              
-              outputpath = Aspose::Cloud::Common::AsposeApp.output_location + @filename
-              Aspose::Cloud::Common::Utils.save_file(outputstream, outputpath)
-              return 'Chart deleted'
-            else
-              return v_output
-            end
+        def delete_chart(worksheet_name, chart_index, folder_name='', storage_type = 'Aspose', storage_name = '')
+          raise 'worksheet_name not specified.' if worksheet_name.empty?
+          raise 'chart_index not specified.' if chart_index.nil?
 
+          str_uri = "#{@base_uri}/worksheets/#{worksheet_name}/charts/#{chart_index}"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          response_stream = RestClient.delete(signed_str_uri, {:accept=>'application/json'})
+          valid_output = Aspose::Cloud::Common::Utils.validate_output(response_stream)
+          valid_output.empty? ? Aspose::Cloud::Common::Utils.download_file(@filename,@filename,folder_name,storage_name,storage_type) : valid_output
         end
-    
-        # Gets ChartArea of a chart 
-        # chart_index
-     
-        def get_chart_area chart_index
 
-            if @filename==''
-              raise 'Base file not specified'
-            end
-            if @worksheet_name==''
-              raise 'Worksheet is not specified'
-            end
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/cells/' + @filename + '/worksheets/' + @worksheet_name + '/charts/' + chart_index.to_s + '/chartArea'
-            signed_uri = Aspose::Cloud::Common::Utils.sign(str_uri)         
-            response = RestClient.get signed_uri, {:accept => 'application/json'}
-            json = JSON.parse(response)
-            return json['ChartArea']
+        def get_chart_area(worksheet_name, chart_index, folder_name='', storage_type = 'Aspose', storage_name = '')
+          raise 'worksheet_name not specified.' if worksheet_name.empty?
+          raise 'chart_index not specified.' if chart_index.nil?
 
+          str_uri = "#{@base_uri}/worksheets/#{worksheet_name}/charts/#{chart_index}/chartArea"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          JSON.parse(RestClient.get(signed_str_uri, {:accept=>'application/json'}))['ChartArea']
         end
-    
-        # Gets fill format of the ChartArea of a chart 
-        # chart_index
-    
-        def get_fill_format chart_index
 
-            if @filename==''
-              raise 'Base file not specified'
-            end
-            if @worksheet_name==''
-              raise 'Worksheet is not specified'
-            end
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/cells/' + @filename + '/worksheets/' + @worksheet_name + '/charts/' + chart_index.to_s + '/chartArea/fillFormat'
-            signed_uri = Aspose::Cloud::Common::Utils.sign(str_uri)    
-            response = RestClient.get signed_uri, {:accept => 'application/json'}
-            json = JSON.parse(response)
-            return json['FillFormat']
+        def get_fill_format(worksheet_name, chart_index, folder_name='', storage_type = 'Aspose', storage_name = '')
+          raise 'worksheet_name not specified.' if worksheet_name.empty?
+          raise 'chart_index not specified.' if chart_index.nil?
 
+          str_uri = "#{@base_uri}/worksheets/#{worksheet_name}/charts/#{chart_index}/chartArea/fillFormat"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          JSON.parse(RestClient.get(signed_str_uri, {:accept=>'application/json'}))['FillFormat']
         end
-    
-        # Gets border of the ChartArea of a chart 
-        # chart_index
-	
-        def get_border chart_index
 
-            if @filename==''
-              raise 'Base file not specified'
-            end
-            if @worksheet_name==''
-              raise 'Worksheet is not specified'
-            end
-            str_uri = Aspose::Cloud::Common::Product.product_uri + '/cells/' + @filename + '/worksheets/' + @worksheet_name + '/charts/' + chart_index.to_s + '/chartArea/border'
-            signed_uri = Aspose::Cloud::Common::Utils.sign(str_uri)        
-            response = RestClient.get signed_uri, {:accept => 'application/json'}
-            json = JSON.parse(response)
-            return json['Line']
+        def get_border(worksheet_name, chart_index, folder_name='', storage_type = 'Aspose', storage_name = '')
+          raise 'worksheet_name not specified.' if worksheet_name.empty?
+          raise 'chart_index not specified.' if chart_index.nil?
 
+          str_uri = "#{@base_uri}/worksheets/#{worksheet_name}/charts/#{chart_index}/chartArea/border"
+          str_uri = Aspose::Cloud::Common::Utils.append_storage(str_uri,folder_name,storage_name,storage_type)
+          signed_str_uri = Aspose::Cloud::Common::Utils.sign(str_uri)
+          JSON.parse(RestClient.get(signed_str_uri, {:accept=>'application/json'}))['Line']
         end
       end
     end
-
-    
   end
 end
